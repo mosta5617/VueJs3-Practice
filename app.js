65d94873b1346ec64786760f0d093797b02ac2f5
Mostafijur-Rahman <mosta5617@gmail.com>
2023-07-20 00:11:58 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,15 @@
+new Vue({
+  el: "#app",
+  data: {
+    title: "Hello Jupiter how are ",
+    array: ['one', 'two', 'three'],
+    robot: {
+      name: "Franky"
+    },
+    gretting: ()=>{
+      return "Hello world";
+    }
+  
+  }
+
+});
