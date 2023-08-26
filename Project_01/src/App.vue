6a6fd3162f4ed7e4cd1b90deec79eefe980badee
Mostafijur-Rahman <mosta5617@gmail.com>
2023-08-26 21:24:27 +0600

    vue project 1 setup

---
+++ Project_01/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <h1>{{ courseName }}s</h1>
+  <p>Course by {{ mentor }} </p>
+  <button @click="count++">+ </button>
+  <p>{{ count }}</p>
+  <button @click="count--">-</button>
+  <p>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint, repellat nulla vitae quae reiciendis assumenda minus consequuntur hic ratione, qui natus commodi tenetur laboriosam exercitationem rem dolore iste accusamus odit. oh 
+  </p>
+</template>
+
+
+
+<script setup>
+  import {ref} from 'vue';
+      const courseName= ref("Vue JS 3 Course Namedfd");
+      const mentor= ref("Mostafijur");
+      let count= ref(0);
+</script>
